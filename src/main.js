771a38cdedf7d2cd29ca53aafd7ef3b71c1c13dc
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 17:49:33 -0300

    list of computers show on screen

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const computers = await fetchProductsList('computador');
+// console.log(computers);
+const sectionEl = document.querySelector('.products');
+computers.forEach((element) => {
+  sectionEl.appendChild(createProductElement(element));
+});
