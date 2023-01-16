1a8a2f762e2bc9123f8e00071b44e2b207b120cc
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 16:29:10 -0300

    fetchProductsList can throw error

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,9 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = (search) => {
   // seu código aqui
+  if (typeof search === 'undefined') {
+    throw new Error('Termo de busca não informado');
+  }
 };
