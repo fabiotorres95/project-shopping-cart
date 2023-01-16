5687419ff6c4b681b8db11a6096ea2e97857e62c
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 16:37:13 -0300

    function can fetch API and returns what we want

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,9 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = (search) => {
+export const fetchProductsList = async (search) => {
   // seu código aqui
   if (typeof search === 'undefined') {
     throw new Error('Termo de busca não informado');
   }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+  const data = await response.json();
+
+  return data.results;
 };
