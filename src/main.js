b2bddea0c5a423189a3b8393902e8cdd949804b7
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 20:13:16 -0300

    added error message for the API

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const sectionEl = document.querySelector('.products');
+
 const showLoadingScreen = () => {
   const message = document.createElement('div');
   message.className = 'loading';
@@ -16,12 +18,16 @@ const removeLoadingScreen = () => {
 };
 
 showLoadingScreen();
-const computers = await fetchProductsList('computador');
-removeLoadingScreen();
-const sectionEl = document.querySelector('.products');
-
-computers.forEach((element) => {
-  sectionEl.appendChild(createProductElement(element));
-});
+try {
+  const computers = await fetchProductsList('computador');
+  computers.forEach((element) => {
+    sectionEl.appendChild(createProductElement(element));
+  });
+  removeLoadingScreen();
+} catch {
+  const message = document.querySelector('.loading');
+  message.className = 'error';
+  message.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+}
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
