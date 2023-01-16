8223f4e01642fb4330bae380c14a0a6835a3b26a
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 16:01:54 -0300

    created the last 2 tests

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -18,6 +18,13 @@ describe('Teste a função fetchProductsList', () => {
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
+  it('testa se o retorno da função é a estrutura de dados esperada', async () => {
+    const result = await fetchProductsList('computador');
+    expect(result).toStrictEqual(computadorSearch);
+  });
+
+  it('testa se ao chamar a função sem argumentos, uma mensagem de erro é retornada', async () => {
+    const result = await fetchProductsList();
+    expect(result).toThrow('Termo de busca não informado');
+  });
 });
