ea28a8d79f5e2b35a3ccee0b2a4f20ab93ab7d0c
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 19:18:22 -0300

    moved the loading functions to main

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,15 +1,3 @@
-const showLoadingScreen = () => {
-  const message = document.createElement('div');
-  message.className = 'loading';
-  message.innerText = 'carregando...';
-  document.querySelector('.products').appendChild(message);
-};
-
-const removeLoadingScreen = () => {
-  const message = document.querySelector('.loading');
-  message.remove();
-};
-
 export const fetchProduct = () => {
   // seu código aqui
 };
@@ -19,10 +7,8 @@ export const fetchProductsList = async (search) => {
   if (typeof search === 'undefined') {
     throw new Error('Termo de busca não informado');
   }
-  showLoadingScreen();
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
   const data = await response.json();
-  removeLoadingScreen();
 
   return data.results;
 };
--- src/main.js
+++ src/main.js
@@ -3,7 +3,21 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const showLoadingScreen = () => {
+  const message = document.createElement('div');
+  message.className = 'loading';
+  message.innerText = 'carregando...';
+  document.querySelector('.products').appendChild(message);
+};
+
+const removeLoadingScreen = () => {
+  const message = document.querySelector('.loading');
+  message.remove();
+};
+
+showLoadingScreen();
 const computers = await fetchProductsList('computador');
+removeLoadingScreen();
 const sectionEl = document.querySelector('.products');
 
 computers.forEach((element) => {
