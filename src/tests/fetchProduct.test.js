d323e4ec47b73f5e346464c9092e18c118050b2f
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-16 20:22:38 -0300

    created 3 tests for fetchProduct

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,17 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('Verifica se fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+
+  it('Verifica se fetchProduct chama a função fetch', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se fetchProduct usa o fetch com o endpoint correto', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
   });
 });
