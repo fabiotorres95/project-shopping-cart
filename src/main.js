f24c7a988b2b8879a3b74c421ab4c2374a43e468
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-18 17:23:55 -0300

    localStorage shows in the cart

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
 import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
 import './style.css';
 
+// variables and aux functions
 const sectionEl = document.querySelector('.products');
-
 const showLoadingScreen = () => {
   const message = document.createElement('div');
   message.className = 'loading';
   message.innerText = 'carregando...';
   document.querySelector('.products').appendChild(message);
 };
-
 const removeLoadingScreen = () => {
   const message = document.querySelector('.loading');
   message.remove();
 };
+const LocalPromises = async (LocalIDs) => {
+  const result = LocalIDs.map((id) => fetchProduct(id));
+  const organizedResult = await Promise.all(result);
+  return organizedResult;
+};
 
+// shows the computers on screen
 showLoadingScreen();
 try {
   const computers = await fetchProductsList('computador');
@@ -28,6 +33,7 @@ try {
     const allProducts = document.querySelectorAll('.product');
     const lastProduct = allProducts[allProducts.length - 1];
 
+    // how each 'add to cart' button will work
     lastProduct.querySelector('.product__add').addEventListener('click', async () => {
       const productID = lastProduct.querySelector('.product__id').innerText;
       saveCartID(productID);
@@ -48,5 +54,23 @@ try {
   message.className = 'error';
   message.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
 }
+// show what's saved in LocalStorage already
+const ids = getSavedCartIDs();
+const products = await LocalPromises(ids);
+const organized = products.map((data) => {
+  const result = {
+    id: data.id,
+    title: data.title,
+    price: data.price,
+    pictures: data.pictures,
+  };
+  return result;
+});
+console.log(organized);
+organized.forEach((newData) => {
+  console.log(newData);
+  document.querySelector('.cart__products')
+    .appendChild(createCartProductElement(newData));
+});
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
