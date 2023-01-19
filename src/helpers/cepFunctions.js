879f0c424860cdd8094e408dd8b9160df930c046
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-19 18:37:28 -0300

    message appears when wrong CEP is typed

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -7,11 +7,16 @@ export const getAddress = async (cep) => {
     const requestBrasil = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
     Promise.any([requestAwesome, requestBrasil])
       .then((data) => data.json()).then((data) => {
-        if (typeof data.address_type !== 'undefined') {
-          res = `${data.address} - ${data.district} - ${data.city} - ${data.state}`;
-          document.querySelector(neededClass).innerHTML = res;
+        if (typeof data.city !== 'undefined') {
+          if (typeof data.address_type !== 'undefined') {
+            res = `${data.address} - ${data.district} - ${data.city} - ${data.state}`;
+            document.querySelector(neededClass).innerHTML = res;
+          } else {
+            res = `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
+            document.querySelector(neededClass).innerHTML = res;
+          }
         } else {
-          res = `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
+          res = 'CEP não encontrado';
           document.querySelector(neededClass).innerHTML = res;
         }
       });
