27958d76446aa3054924edf4c9605786753008eb
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-19 19:57:40 -0300

    changed syntax due to cy issues

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -2,29 +2,29 @@ export const getAddress = async (cep) => {
   // seu código aqui
   const neededClass = '.cart__address';
   let res = '';
-  try {
-    const requestAwesome = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
-    const requestBrasil = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
-    Promise.any([requestAwesome, requestBrasil])
-      .then((data) => data.json()).then((data) => {
-        if (typeof data.city !== 'undefined') {
-          if (typeof data.address_type !== 'undefined') {
-            res = `${data.address} - ${data.district} - ${data.city} - ${data.state}`;
-            document.querySelector(neededClass).innerHTML = res;
-          } else {
-            res = `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
-            document.querySelector(neededClass).innerHTML = res;
-          }
+  // try {
+  const requestAwesome = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+  const requestBrasil = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+  Promise.any([requestAwesome, requestBrasil])
+    .then((data) => data.json())
+    .then((data) => {
+      if (!data.status) {
+        if (typeof data.address_type !== 'undefined') {
+          res = `${data.address} - ${data.district} - ${data.city} - ${data.state}`;
+          document.querySelector(neededClass).innerHTML = res;
         } else {
-          res = 'CEP não encontrado';
+          res = `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
           document.querySelector(neededClass).innerHTML = res;
         }
-      });
-  } catch {
-    res = 'CEP não encontrado';
-    document.querySelector(neededClass).innerHTML = res;
-  }
-  // document.querySelector(neededClass).innerHTML = res;
+      } else {
+        res = 'CEP não encontrado';
+        document.querySelector(neededClass).innerHTML = res;
+      }
+    })
+    .catch(() => {
+      res = 'CEP não encontrado';
+      document.querySelector(neededClass).innerHTML = res;
+    });
 };
 
 export const searchCep = async () => {
